bd2f1b1ef64177994bba80cbe572eaa93af81a46
28ede1 <e.ekpenyong263@gmail.com>
2026-06-22 14:14:38 -0700

    Initial Commit

--- student-store-api/src/server.js
+++ student-store-api/src/server.js
@@ -1,1 +1,21 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT;
+
+//middleware
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Student Store Web App Now Runnings.")
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
 
